a0e3c1d99c024e0ff2575f304cd423686df76177
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-04-16 13:56:03 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio8/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar. */
+
+using System;
+
+namespace Exercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, result;
+
+            Console.WriteLine("DIGITE O NÚMERO: ");
+            number = int.Parse(Console.ReadLine());
+
+            result = number % 2;
+
+            if (result == 0)
+            {
+                Console.WriteLine("PAR");
+            } else
+            {
+                Console.WriteLine("IMPAR");
+            }
+        }
+    }
+}
